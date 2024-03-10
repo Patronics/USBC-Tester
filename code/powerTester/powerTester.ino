94723b5608fc44613d212c20e538b0e4b4eccbe3
Patrick Leiser <patrick27leiser@yahoo.com>
2024-03-10 03:31:47 -0700

    make remapLeds function to set LED bitmap to cleaner order

--- code/powerTester/powerTester.ino
+++ code/powerTester/powerTester.ino
@@ -1,3 +1,8 @@
+//USB C Power tester
+//Patrick Leiser, March 2024
+
+//if U1 and U2 are unpopulated, green-wire U1 pin 14 to U2 pin 9 and define USB_SHIFT_REG_BYPASS
+#define USB_SHIFT_REG_BYPASS
 
 
 
@@ -30,34 +35,79 @@ void loop() {
 //do a scan to demonstrate that all LEDs are functional
 void scanLeds(int loopDelay){
   digitalWrite(internalLED, !digitalRead(internalLED));
-  for(int i=1; i<pow(2,28); i=i*2){
+  for(int i=1; i<pow(2,29); i=i*2){
     sendBits(i, 0, true, false);
     delay(loopDelay);
   }
 
 }
 
-//LEDBits = first 29 bits to shift out (MSB 3 bits unused)
-//usbBits = remaining 19 values to shift out (important to note that the 3 LSB WILL be set if ledEnable is true even if usbEnable is false)
+/*ledBits - software mapping:
+0x000000FF > power
+0x00000F00 > usb 2
+0x000FF000 > usb 3
+0x00F00000 > other
+0x1F000000 > status
+0xE0000000 > unused (ignored)
+
+usbBits = remaining 19 values to shift out (important to note that the 3 LSB WILL be set if ledEnable is true even if usbEnable is false)
+*/
 void sendBits(long ledBits, long usbBits, bool ledEnable, bool usbEnable){
   digitalWrite(shiftOEUSB,HIGH);
   digitalWrite(shiftOELEDs,HIGH);
-  for(int i=18; i>=3; i--){   //shift out most of USB
-    shiftOut(usbBits, i);
-  }
-  for(int i=28; i>=24; i--){   //shift out last few LEDs
+  ledBits = remapLeds(ledBits);
+
+  for(int i=24; i<29; i++){   //shift out last few LEDs
     shiftOut(ledBits, i);
   }
-  for(int i=2; i>=0; i--){   //shift out last few USB
+#ifdef USB_SHIFT_REG_BYPASS
+    for(int i=0; i<3; i++){   //shift out USB's 3 LSB only
     shiftOut(usbBits, i);
   }
-  for(int i=23; i>=0; i--){  //shift out most of LEDs
+#else
+  for(int i=0; i<18; i++){   //shift out USB
+    shiftOut(usbBits, i);
+  }
+#endif
+  for(int i=0; i<24; i++){  //shift out most of LEDs
     shiftOut(ledBits, i);
   }
 
 
   digitalWrite(shiftOEUSB,!usbEnable); //OE is active low, so invert
   digitalWrite(shiftOELEDs,!ledEnable);
+}
+
+/*
+software mapping:
+0x000000FF > power
+0x00000F00 > usb 2
+0x000FF000 > usb 3
+0x00F00000 > other
+0x1F000000 > status
+
+hardware mapping:
+0x00FF0000 > power
+0x0000F000 > usb 2
+0x00000F87 > usb 3
+0x00000F00 > usb 3 - bit 0-3
+0x00000080 > usb 3 - bit 4
+0x00000007 > usb 3 - bit 5-7
+0x00000078 > other
+0x1F000000 > status
+*/
+
+//remap bit order of LEDs to simplify understanding
+long remapLeds(long cleanMapping){
+  
+  long remapped = ((cleanMapping &   0xFF) << 16); //Power
+  remapped += ((cleanMapping &     0x0F00) << 4);  //usb 2
+  remapped += ((cleanMapping &    0x0F000) >> 4);  // usb 3 bit 0-3
+  remapped += ((cleanMapping &    0x10000) >> 9);  // usb 3 bit 4
+  remapped += ((cleanMapping &    0xE0000) >> 17); // usb 3 bit 5-7
+  remapped += ((cleanMapping &   0xF00000) >> 17); // other
+  remapped += (cleanMapping & 0x1F000000);       //status
+  return remapped;
 }
 
 void shiftOut(long word, int chosenBit){
